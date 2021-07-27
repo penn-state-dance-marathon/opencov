aecdad8dd5e8ce84e6b77ef016a9a257f8a955c3
Noah Gearhart <excelsiorvfx@gmail.com>
2021-07-27 19:03:17 -0400

    Env variables

--- config/local.sample.exs
+++ config/local.sample.exs
@@ -2,15 +2,15 @@ use Mix.Config
 
 config :opencov, Opencov.Endpoint,
   http: [port: 4000],
-  url: [scheme: "http", host: "coverage.thon.org", port: 80],
-  secret_key_base: "my-super-secret-key-base-with-64-characters-so-that-i-dont-get-an-error"
+  url: [scheme: "SCHEME", host: "URLHOST", port: PORT],
+  secret_key_base: "SECRET_KEY"
 
 config :opencov, Opencov.Repo,
   adapter: Ecto.Adapters.MySQL,
   database: "opencov_prod",
-  username: "root",
-  password: "coverage",
-  hostname: "db"
+  username: "DB_USERNAME",
+  password: "DB_PASSWORD",
+  hostname: "DB_HOSTNAME"
 
 config :opencov, :email,
   sender: "OpenCov <info@example.com>",
@@ -30,8 +30,8 @@ config :opencov, :openid_connect_providers,
   keycloak: [
     discovery_document_uri: "https://access.thon.org/auth/realms/THON/.well-known/openid-configuration",
     client_id: "opencov",
-    client_secret: "ex",
-    redirect_uri: "http://ngearhart.myqnapcloud.com:8000/login/callback",
+    client_secret: "KEYCLOAK_SECRET",
+    redirect_uri: "SCHEME://URLHOST/login/callback",
     response_type: "code",
     scope: "openid email profile"
   ]
