f836aea11e64a543f9e60d45273c25926bd26de7
Noah Gearhart <excelsiorvfx@gmail.com>
2021-07-27 09:32:38 -0400

    Login is done

--- config/local.sample.exs
+++ config/local.sample.exs
@@ -28,7 +28,7 @@ config :opencov, :openid_connect_providers,
   keycloak: [
     discovery_document_uri: "https://access.thon.org/auth/realms/THON/.well-known/openid-configuration",
     client_id: "opencov",
-    client_secret: "ex",
+    client_secret: "f4449cc0-f5a3-4005-936d-2f4a80b20cf4",
     redirect_uri: "http://ngearhart.myqnapcloud.com:8000/login/callback",
     response_type: "code",
     scope: "openid email profile"
@@ -39,6 +39,10 @@ config :opencov,
     "ec.Executive",
     "ec.Technology",
     "tech.LeadSystemsAdministrator"
+  ],
+  permitted_roles: [
+    "ec.Executive",
+    "tech.all"
   ]
 
 config :phoenix_live_reload,
--- lib/opencov/authentication.ex
+++ lib/opencov/authentication.ex
@@ -8,10 +8,14 @@ defmodule Opencov.Authentication do
 
   @user_id_key :user_id
 
-  def login(conn, email, name) do
+  def login(conn, email, name, roles) do
     userToLogin = Repo.get_by(User, email: email)
+    
+    admin_roles = Application.get_all_env(:opencov)[:admin_roles]
+    assign_admin = not MapSet.disjoint?(MapSet.new(admin_roles), MapSet.new(roles))
+
     if is_nil(userToLogin) do
-      changeset = UserManager.changeset(%User{}, %{email: email, name: name, admin: true})
+      changeset = UserManager.changeset(%User{}, %{email: email, name: name, admin: assign_admin})
       case Repo.insert(changeset) do
         {:ok, user} = res ->
           userToLogin = user
@@ -20,6 +24,8 @@ defmodule Opencov.Authentication do
           err
       end
       UserService.finalize_confirmation!(userToLogin)
+    else
+      UserManager.changeset(userToLogin, %{admin: assign_admin}) |> Repo.update!
     end
     put_session(conn, user_id_key(), userToLogin.id)
   end
